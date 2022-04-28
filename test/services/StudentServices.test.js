0417c95c937a61da06b7335cc3c1b86c6af0f4dd
Gera097 <84435213+Gera097@users.noreply.github.com>
2022-04-28 16:25:02 -0500

    Unit test - getAllStudents, getStudentsEmailByCertifiedStatus

---
+++ test/services/StudentServices.test.js
@@ -0,0 +1,75 @@
+const StudentServices = require("./../../lib/services/StudentServices")
+
+describe("Unit test for StudentServices class", () => {
+
+    test("get a list with all the students and all fields", () => {
+        const student_list = [{
+            "id": "6264d5d8aa87c9330234cbc2",
+            "name": "Hollie",
+            "email": "Jewell@visualpartnership.xyz",
+            "credits": 376,
+            "enrollments": [
+              "Visual Thinking Intermedio",
+              "Visual Thinking Básico"
+            ],
+            "previousCourses": 5,
+            "haveCertification": true
+          },{
+            "id": "6264d5d82eb0f4917bd0d332",
+            "name": "Clare",
+            "email": "Hays@visualpartnership.xyz",
+            "credits": 227,
+            "enrollments": [
+              "Visual Thinking Intermedio",
+              "Visual Thinking Básico"
+            ],
+            "previousCourses": 2,
+            "haveCertification": true
+          }]
+        
+        const result = StudentServices.getAllStudents(student_list)
+
+        expect(result).toEqual(student_list)
+    })
+
+    test("get a list of emails of students depending on their certified status", () => {
+        const student_list = [{
+            "id": "6264d5d8aa87c9330234cbc2",
+            "name": "Hollie",
+            "email": "Jewell@visualpartnership.xyz",
+            "credits": 376,
+            "enrollments": [
+              "Visual Thinking Intermedio",
+              "Visual Thinking Básico"
+            ],
+            "previousCourses": 5,
+            "haveCertification": true
+          },{
+            "id": "6264d5d82eb0f4917bd0d332",
+            "name": "Clare",
+            "email": "Hays@visualpartnership.xyz",
+            "credits": 227,
+            "enrollments": [
+              "Visual Thinking Intermedio",
+              "Visual Thinking Básico"
+            ],
+            "previousCourses": 2,
+            "haveCertification": false
+          },
+          {
+            "id": "6264d5d8cda17de0d2e9f236",
+            "name": "Fuentes",
+            "email": "Sharlene@visualpartnership.xyz",
+            "credits": 210,
+            "enrollments": [
+              "Visual Thinking Avanzado"
+            ],
+            "previousCourses": 10,
+            "haveCertification": true
+          },
+        ]
+        const status = true
+        const result = StudentServices.getStudentEmailByCertifiedStatus(student_list, status)
+        expect(result).toEqual(["Jewell@visualpartnership.xyz", "Sharlene@visualpartnership.xyz"])
+    })
+})
