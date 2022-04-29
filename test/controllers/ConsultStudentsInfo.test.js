a376b590ba431f05f08b24111a56df37369647f3
Gera097 <84435213+Gera097@users.noreply.github.com>
2022-04-29 01:02:59 -0500

    Unit Test - controller getStudentsAboveCredits

--- test/controllers/ConsultStudentsInfo.test.js
+++ test/controllers/ConsultStudentsInfo.test.js
@@ -7,10 +7,17 @@ describe("Unit test for ConsultStudentInfo class", () => {
         expect(result.length).toBe(51)
     })
 
-    test("get the list of students that have more than the specified credits", () => {
+    test("get a list of emails of students depending on their certified status", () => {
         let haveCertification = true
         const emailRegex = [expect.stringMatching(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i)]
         const result = ConsultStudentsInfo.getStudentsEmailByCertifiedStatus(haveCertification)
         expect(result).toEqual(expect.arrayContaining(emailRegex))
+    })
+
+    test("get the list of students that have more than the specified credits", () => {
+        let credits = 500
+        const result = ConsultStudentsInfo.getStudentsAboveCredits(credits)
+        const studentCredits = result.map((student) => student.credits)
+        expect(Math.min(...studentCredits)).toBeGreaterThan(credits)
     })
 })
