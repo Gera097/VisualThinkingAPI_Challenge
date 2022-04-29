e68cfbaf058f86249875d04936cf24891590434d
Gera097 <84435213+Gera097@users.noreply.github.com>
2022-04-29 01:49:26 -0500

    Arreglando estilo con ESLint

--- test/controllers/ConsultStudentsInfo.test.js
+++ test/controllers/ConsultStudentsInfo.test.js
@@ -1,23 +1,23 @@
-const ConsultStudentsInfo = require("./../../lib/controllers/ConsultStudentsInfo")
+const ConsultStudentsInfo = require("./../../lib/controllers/ConsultStudentsInfo");
 
 describe("Unit test for ConsultStudentInfo class", () => {
 
     test("get all students", () => {
-        const result = ConsultStudentsInfo.getAllStudents()
-        expect(result.length).toBe(51)
-    })
+        const result = ConsultStudentsInfo.getAllStudents();
+        expect(result.length).toBe(51);
+    });
 
     test("get a list of emails of students depending on their certified status", () => {
-        let haveCertification = true
-        const emailRegex = [expect.stringMatching(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i)]
-        const result = ConsultStudentsInfo.getStudentsEmailByCertifiedStatus(haveCertification)
-        expect(result).toEqual(expect.arrayContaining(emailRegex))
-    })
+        let haveCertification = true;
+        const emailRegex = [expect.stringMatching(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i)];
+        const result = ConsultStudentsInfo.getStudentsEmailByCertifiedStatus(haveCertification);
+        expect(result).toEqual(expect.arrayContaining(emailRegex));
+    });
 
     test("get the list of students that have more than the specified credits", () => {
-        let credits = 500
-        const result = ConsultStudentsInfo.getStudentsAboveCredits(credits)
-        const studentCredits = result.map((student) => student.credits)
-        expect(Math.min(...studentCredits)).toBeGreaterThan(credits)
-    })
-})
+        let credits = 500;
+        const result = ConsultStudentsInfo.getStudentsAboveCredits(credits);
+        const studentCredits = result.map((student) => student.credits);
+        expect(Math.min(...studentCredits)).toBeGreaterThan(credits);
+    });
+});
--- test/services/StudentServices.test.js
+++ test/services/StudentServices.test.js
@@ -1,4 +1,4 @@
-const StudentServices = require("./../../lib/services/StudentServices")
+const StudentServices = require("./../../lib/services/StudentServices");
 
 describe("Unit test for StudentServices class", () => {
 
@@ -9,28 +9,28 @@ describe("Unit test for StudentServices class", () => {
             "email": "Jewell@visualpartnership.xyz",
             "credits": 376,
             "enrollments": [
-              "Visual Thinking Intermedio",
-              "Visual Thinking Básico"
+                "Visual Thinking Intermedio",
+                "Visual Thinking Básico"
             ],
             "previousCourses": 5,
             "haveCertification": true
-          },{
+        },{
             "id": "6264d5d82eb0f4917bd0d332",
             "name": "Clare",
             "email": "Hays@visualpartnership.xyz",
             "credits": 227,
             "enrollments": [
-              "Visual Thinking Intermedio",
-              "Visual Thinking Básico"
+                "Visual Thinking Intermedio",
+                "Visual Thinking Básico"
             ],
             "previousCourses": 2,
             "haveCertification": true
-          }]
+        }];
         
-        const result = StudentServices.getAllStudents(student_list)
+        const result = StudentServices.getAllStudents(student_list);
 
-        expect(result).toEqual(student_list)
-    })
+        expect(result).toEqual(student_list);
+    });
 
     test("get a list of emails of students depending on their certified status", () => {
         const student_list = [{
@@ -39,39 +39,39 @@ describe("Unit test for StudentServices class", () => {
             "email": "Jewell@visualpartnership.xyz",
             "credits": 376,
             "enrollments": [
-              "Visual Thinking Intermedio",
-              "Visual Thinking Básico"
+                "Visual Thinking Intermedio",
+                "Visual Thinking Básico"
             ],
             "previousCourses": 5,
             "haveCertification": true
-          },{
+        },{
             "id": "6264d5d82eb0f4917bd0d332",
             "name": "Clare",
             "email": "Hays@visualpartnership.xyz",
             "credits": 227,
             "enrollments": [
-              "Visual Thinking Intermedio",
-              "Visual Thinking Básico"
+                "Visual Thinking Intermedio",
+                "Visual Thinking Básico"
             ],
             "previousCourses": 2,
             "haveCertification": false
-          },
-          {
+        },
+        {
             "id": "6264d5d8cda17de0d2e9f236",
             "name": "Fuentes",
             "email": "Sharlene@visualpartnership.xyz",
             "credits": 210,
             "enrollments": [
-              "Visual Thinking Avanzado"
+                "Visual Thinking Avanzado"
             ],
             "previousCourses": 10,
             "haveCertification": true
-          },
-        ]
-        const status = "true"
-        const result = StudentServices.getStudentEmailByCertifiedStatus(student_list, status)
-        expect(result).toEqual(["Jewell@visualpartnership.xyz", "Sharlene@visualpartnership.xyz"])
-    })
+        },
+        ];
+        const status = "true";
+        const result = StudentServices.getStudentEmailByCertifiedStatus(student_list, status);
+        expect(result).toEqual(["Jewell@visualpartnership.xyz", "Sharlene@visualpartnership.xyz"]);
+    });
 
     test("get the list of students that have more than the specified credits", () => {
         const student_list = [{
@@ -80,37 +80,37 @@ describe("Unit test for StudentServices class", () => {
             "email": "Jewell@visualpartnership.xyz",
             "credits": 376,
             "enrollments": [
-              "Visual Thinking Intermedio",
-              "Visual Thinking Básico"
+                "Visual Thinking Intermedio",
+                "Visual Thinking Básico"
             ],
             "previousCourses": 5,
             "haveCertification": true
-          },{
+        },{
             "id": "6264d5d82eb0f4917bd0d332",
             "name": "Clare",
             "email": "Hays@visualpartnership.xyz",
             "credits": 501,
             "enrollments": [
-              "Visual Thinking Intermedio",
-              "Visual Thinking Básico"
+                "Visual Thinking Intermedio",
+                "Visual Thinking Básico"
             ],
             "previousCourses": 2,
             "haveCertification": false
-          },
-          {
+        },
+        {
             "id": "6264d5d8cda17de0d2e9f236",
             "name": "Fuentes",
             "email": "Sharlene@visualpartnership.xyz",
             "credits": 750,
             "enrollments": [
-              "Visual Thinking Avanzado"
+                "Visual Thinking Avanzado"
             ],
             "previousCourses": 10,
             "haveCertification": true
-          },
-        ]
-        let credits = 500
-        let result = StudentServices.getStudentsAboveCredits(student_list, credits)
+        },
+        ];
+        let credits = 500;
+        let result = StudentServices.getStudentsAboveCredits(student_list, credits);
 
         expect(result).toEqual([{
             "id": "6264d5d82eb0f4917bd0d332",
@@ -118,26 +118,26 @@ describe("Unit test for StudentServices class", () => {
             "email": "Hays@visualpartnership.xyz",
             "credits": 501,
             "enrollments": [
-              "Visual Thinking Intermedio",
-              "Visual Thinking Básico"
+                "Visual Thinking Intermedio",
+                "Visual Thinking Básico"
             ],
             "previousCourses": 2,
             "haveCertification": false
-          },
-          {
+        },
+        {
             "id": "6264d5d8cda17de0d2e9f236",
             "name": "Fuentes",
             "email": "Sharlene@visualpartnership.xyz",
             "credits": 750,
             "enrollments": [
-              "Visual Thinking Avanzado"
+                "Visual Thinking Avanzado"
             ],
             "previousCourses": 10,
             "haveCertification": true
-          }])
-          result = StudentServices.getStudentsAboveCredits(student_list, 300)
-        expect(result).toEqual(student_list)
+        }]);
+        result = StudentServices.getStudentsAboveCredits(student_list, 300);
+        expect(result).toEqual(student_list);
         
-    })
+    });
 
-})
+});
