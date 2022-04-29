c59026587489410fbafcbc9ca9bf8ff9ae7366f8
Gera097 <84435213+Gera097@users.noreply.github.com>
2022-04-29 00:25:16 -0500

    Unit Test - controller getStudentsEmailByCertifiedStatus

--- test/controllers/ConsultStudentsInfo.test.js
+++ test/controllers/ConsultStudentsInfo.test.js
@@ -1,9 +1,16 @@
 const ConsultStudentsInfo = require("./../../lib/controllers/ConsultStudentsInfo")
 
 describe("Unit test for ConsultStudentInfo class", () => {
-    
+
     test("get all students", () => {
         const result = ConsultStudentsInfo.getAllStudents()
         expect(result.length).toBe(51)
+    })
+
+    test("get the list of students that have more than the specified credits", () => {
+        let haveCertification = true
+        const emailRegex = [expect.stringMatching(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i)]
+        const result = ConsultStudentsInfo.getStudentsEmailByCertifiedStatus(haveCertification)
+        expect(result).toEqual(expect.arrayContaining(emailRegex))
     })
 })
